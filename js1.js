9cec0878b5c34654cca5d9540e2e490dd60a7ada
Sanju <sanzu.tmg07@gmail.com>
2022-02-21 22:41:06 +0545

    new file

---
+++ js1.js
@@ -0,0 +1,15 @@
+// const arr = ["Banana", "Orange", "Apple", "Mango"];
+
+// arr.sort()
+
+// console.log(arr)
+
+// const cars = [
+//     {type:"Toyota", year:2016},
+//     {type:"Mazda", year:2001},
+//     {type:"BMW", year:2010}
+//   ];
+  
+//   cars.sort()
+// //   cars.sort(function(a, b){return a.year - b.year});
+//  console.log(cars)
